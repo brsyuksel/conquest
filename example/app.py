24bf69c9ec4492e55616c11a4f3e47cc5b27a49d
Baris Yuksel <brsyuksel@gmail.com>
2015-05-17 17:05:08 +0000

    example tornado web application and conquest.js file
    Signed-off-by: Baris Yuksel <brsyuksel@gmail.com>

---
+++ example/app.py
@@ -0,0 +1,130 @@
+__author__ = 'baris'
+
+from tornado.web import Application, RequestHandler
+from tornado.ioloop import IOLoop
+
+class BaseHandler(RequestHandler):
+    def initialize(self):
+        self.set_header("Content-Type", "application/json")
+        print(self.xsrf_token)
+
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
+
+class RootHandler(BaseHandler):
+    def get(self, *args, **kwargs):
+        self.write({
+            "user": self.current_user.decode("utf-8"),
+        })
+        self.finish()
+
+class AuthenticationHandler(BaseHandler):
+    def post(self, *args, **kwargs):
+        u = self.get_argument("user", None)
+        p = self.get_argument("pass", None)
+        if u == "root" and p == "toor":
+            self.set_secure_cookie("user", "root")
+            self.set_status(200)
+            self.write({
+                "message": "ok"
+            })
+        else:
+            self.set_status(401)
+            self.write({
+                "error": "invalid credentials",
+                "message": "not ok"
+            })
+
+    def get(self, *args, **kwargs):
+        if self.current_user:
+            self.set_status(200)
+            self.write({
+                "message": "welcome back!",
+                "meta": {"user": self.current_user.decode("utf-8")}
+            })
+        else:
+            self.set_status(401)
+            self.write({
+                "error": "unauthorized",
+                "message": "login first"
+            })
+
+    def delete(self, *args, **kwargs):
+        if not self.current_user:
+            self.set_status(403)
+            self.finish()
+        self.clear_all_cookies()
+        self.set_status(200)
+
+class ForbiddenHandler(BaseHandler):
+    def get(self, *args, **kwargs):
+        if not self.current_user:
+            self.write({
+                "error": "unauthorized",
+                "message": "login first"
+            })
+            self.set_status(403)
+            self.finish()
+
+        self.write({
+            "user": self.current_user.decode("utf-8")
+        })
+        self.set_status(200)
+
+class QueryPostHandler(BaseHandler):
+    def get(self, *args, **kwargs):
+        q = self.get_query_argument("q", None)
+        self.write({"q": q})
+
+    def post(self, *args, **kwargs):
+        q = self.get_argument("q", None)
+        self.write({"q": q})
+
+
+class FileUploadHandler(BaseHandler):
+    def post(self, *args, **kwargs):
+        f = self.request.files.get("file", None)
+        if f is None:
+            self.set_status(400)
+            self.write({
+                "error": "file"
+            })
+            self.finish()
+
+        f = f[0]
+        if f["content_type"] == "text/markdown":
+            self.write({
+                "name": f["filename"]
+            })
+        else:
+            self.set_status(415)
+            self.write({"error": f["content_type"]})
+
+
+class HeadersHandler(BaseHandler):
+    def get(self, *args, **kwargs):
+        self.write({"asdasdasd" : 1})
+        self.set_etag_header()
+        if self.check_etag_header():
+            self._write_buffer = []
+            self.set_status(304)
+            return
+        self.finish()
+
+class MyApp(Application):
+    def __init__(self):
+        super(MyApp, self).__init__(handlers=[
+            (r"/", RootHandler),
+            (r"/auth", AuthenticationHandler),
+            (r"/forbidden", ForbiddenHandler),
+            (r"/searchlike", QueryPostHandler),
+            (r"/static", HeadersHandler),
+            (r"/file", FileUploadHandler),
+        ], **{
+            "cookie_secret": "sw28x*+&foyw3_&9*n*)tl(uj8m9az=udk+od&1x+o@q",
+            "xsrf_cookies": True,
+            "auto_reload": True
+        })
+
+MyApp().listen(2297)
+IOLoop.instance().start()
---
+++ example/conquest-ex.js
@@ -0,0 +1,142 @@
+conquest
+  .Host("http://10.0.2.2:2297")
+  .Requests(100)
+  .Users(10, function(users){
+    users
+      .Every(function(user){
+        /*
+        * first reach root path
+        */
+        user
+          .Do("GET", "/")
+          .Response
+            .StatusCode(200)
+            .Header("Server", "TornadoServer/4.1")
+            .Header("Content-Length", "16")
+        ;
+
+        /*
+        * Try signing in with invalid credentials
+        */
+
+        user
+          .Do("POST", "/auth")
+          .Body({
+            "user": "non-exists",
+            "pass": "secret",
+            "_xsrf": function(fetch){ return fetch.FromCookie("_xsrf"); },
+          })
+          .Response
+            .StatusCode(401)
+        ;
+
+        /*
+        * Sign in
+        */
+
+        user
+          .Do("POST", "/auth")
+          .Body({
+            "user": "root",
+            "pass": "toor",
+            "_xsrf": function(fetch){ return fetch.FromCookie("_xsrf"); },
+          })
+        ;
+      })
+      .Then(function(user){
+        /*
+        * reach forbidden profile zone
+        */
+        user
+          .Do("GET", "/forbidden")
+          .Response
+            .StatusCode(200)
+        ;
+
+        /*
+        * try reach forbidden profile zone without cookies
+        */
+        user
+          .Do("GET", "/forbidden")
+          .ClearCookies()
+          .Response
+            .StatusCode(403)
+        ;
+
+        /*
+        * Query via GET
+        */
+        user
+          .Do("GET", "/searchlike")
+          .Body({
+            "q": "conquest"
+          })
+        ;
+
+        /*
+        * Query via POST
+        */
+        user
+          .Do("POST", "/searchlike")
+          .Body({
+            "q": "conquest",
+            "_xsrf": function(fetch){ return fetch.FromCookie("_xsrf"); }
+          })
+        ;
+
+        /*
+        * Fetch static file and its etag
+        */
+        user
+          .Do("GET", "/static")
+          .Response
+            .StatusCode(200)
+        ;
+        
+        /*
+        * Check If-None-Match with etag
+        */
+        user
+          .Do("GET", "/static")
+          .Header("If-None-Match", function(fetch){ return fetch.FromHeader("Etag"); })
+          .Response
+            .StatusCode(304)
+        ;
+
+        /*
+        * Try upload a file
+        */
+        user
+          .Do("POST", "/file")
+          .Body({
+            "file": function(fetch){ return fetch.FromDisk("test_files/", "text/markdown"); },
+            "_xsrf": function(fetch){ return fetch.FromCookie("_xsrf"); }
+          })
+          .Response
+            .StatusCode(200)
+        ;
+
+        /*
+        * Try upload invalid mime type
+        */
+        user
+          .Do("POST", "/file")
+          .Body({
+            "file": function(fetch){ return fetch.FromDisk("test_files/", "image/png"); },
+            "_xsrf": function(fetch){ return fetch.FromCookie("_xsrf"); }
+          })
+          .Response
+            .StatusCode(415)
+        ;
+      })
+      .Finally(function(user){
+        /*
+        * Everybody should go home
+        */
+        user
+          .Do("DELETE", "/auth")
+          .Response
+            .StatusCode(200)
+        ;
+      })
+  });
